6a894b5694c53a871609eeed7985c3dc97bf1b6a
IT-DEPARTMENT-B2B <itdepartmentb2b@gmail.com>
2025-07-26 00:23:16 +0530

    Initial commit

--- teamConfig.js
+++ teamConfig.js
@@ -1,16 +1,16 @@
-// teamConfig.js
-
 const teamMapping = {
-  'FAIZAL': [ 'Ahmad', 'Michael', 'John', 'Ray', 'Archie', 'Ricky'],
-  'VICTOR': [ 'Mohsin', 'Peter'],
- 
+  'FAIZAL': ['Ahmad', 'Michael', 'John', 'Ray', 'Archie', 'Ricky'],
+  'VICTOR': ['Mohsin', 'Peter'],
   'ROBIN': ['Adam', 'Sam', 'Marcel', 'Fredie'],
   'MARCUS': ['Keev', 'Rody', 'Abraham', 'Ryan', 'Atin'],
-  'DAISY': [ 'Tia', 'Zella'],
-  'AVA': [ 'Gloria', 'Jennie', 'Sonya', 'Lily', 'Linza'],
+  'DAISY': ['Tia', 'Zella'],
+  'AVA': ['Gloria', 'Jennie', 'Sonya', 'Lily', 'Linza'],
   'BRIAN': ['Jason', 'Neeraj', 'Allen'],
   'TONY': ['Rick C', 'Leo M', 'Richard M'],
   'LUKE': ['Omar', 'Alex', 'James L']
 };
+
+// Add MARK with access to all users across all teams
+teamMapping['MARK'] = Object.values(teamMapping).flat();
 
 module.exports = teamMapping;
