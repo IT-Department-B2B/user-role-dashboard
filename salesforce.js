070126ca2e8cf5463a6a525dac7d7e34cce59e46
IT Department <itdepartmentb2b@gmail.com>
2026-01-06 19:48:29 +0530

    Update Salesforce credentials
    Updated password and security token for Salesforce connection.

--- salesforce.js
+++ salesforce.js
@@ -2,8 +2,8 @@
 const jsforce = require('jsforce');
 
 const username = 'pramod@retrotech.in';
-const password = 'Admin@Retrotech#2029';
-const securityToken = '9Q5hSFyhRhWzkhHhRaNVHbiWE'; // or latest
+const password = 'Admin@Retrotech#2030';
+const securityToken = 'ClnHTjDCpZWc5byWJC5D0V02O'; // or latest
 const loginUrl = 'https://login.salesforce.com';
 
 const conn = new jsforce.Connection({ loginUrl });
